0340735f400bd07b5785e1cfa587fc3161d297b3
giselaYasmin <80352971+giselaYasmin@users.noreply.github.com>
2023-02-28 09:39:50 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/civ/', //nombre del repositorio en github
+  base: '/apiE/', //nombre del repositorio en github
   plugins: [vue()],
 })
